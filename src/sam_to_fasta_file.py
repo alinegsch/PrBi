2a4fc9e95017a61db556826b3be4b04126a484b6
Aline <aline.gschwind@stud.unibas.ch>
2021-12-17 00:51:40 +0100

    exercise two: sam to fasta file programm added

---
+++ src/sam_to_fasta_file.py
@@ -0,0 +1,21 @@
+import parse_fasta
+
+
+def sam_to_fasta_file(path):
+    converted_fasta = 'fasta_converted_from_sam_file.fasta'
+    with open(converted_fasta, 'w') as out:
+        with open(path, 'r') as fh:
+            data = fh.readlines()
+        for line in data:
+            if line[0] != '@':
+                sam_split = line.split("\t")
+                if sam_split[9][0] == 'A' or sam_split[9][0] == 'C' or sam_split[9][0] == 'G' or sam_split[9][0] == 'T':
+                    out.write(">" + sam_split[0] + "\n")
+                    out.write(sam_split[9] + "\n")
+
+
+if __name__ == '__main__':
+    sam_to_fasta_file('Aligned.out.sam')
+    dict = parse_fasta.map_reads('fasta_converted_from_sam_file.fasta',
+                                 r"/Users/Aline/Desktop/21HS/21HS PrBi/PrBi Alexander Kanitz/RNA-Seq/Test files/genome.fasta")
+    print(dict)
